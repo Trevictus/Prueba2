ddb5fa7ac7fa012f0fdaa235d308dce5fe6e1bcc
Trevictus <vgomtej353@g.educaand.es>
2024-02-20 20:19:49 +0100

    Prueba2 aprendiendo

---
+++ src/Main.kt
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//Aquí voy a hacer un push con esto para probar
+}
