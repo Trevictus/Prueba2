048fe0ec5ea975e60ec4411febab7a0b6a953881
Trevictus <vgomtej353@g.educaand.es>
2024-02-20 20:36:07 +0100

    Cambio a master

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,5 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 //Aquí voy a hacer un push con esto para probar, estoy intentando aprender algo que el de entornos no explica ni con su vida en juego, este es el tercer cambio para las branchs.
+println("Cambio a master")
 }
