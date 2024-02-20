04f675156c23d4be4a597870c884659fbcc07abd
Trevictus <vgomtej353@g.educaand.es>
2024-02-20 20:25:22 +0100

    Prueba2 aprendiendo2

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-//Aquí voy a hacer un push con esto para probar, estoy intentando aprender algo que el de entornos no explica ni con su vida en juego
+//Aquí voy a hacer un push con esto para probar, estoy intentando aprender algo que el de entornos no explica ni con su vida en juego, este es el tercer cambio para las branchs.
 }
